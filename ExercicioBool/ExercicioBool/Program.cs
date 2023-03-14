b5c93c9267a48234d28a4249f1092c549dac3cc2
Joanne-Emilly <124714961+Joanne-Emilly@users.noreply.github.com>
2023-03-14 12:24:30 -0300

    ExeciciosAtualizados

---
+++ ExercicioBool/ExercicioBool/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioBool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            for (i = 0; i > 10; i++) ;
+            {
+                if (i > 0)
+                {
+                    continue;
+             }
+                Console.WriteLine("i = 0", i);
+
+         }
+   
+ 
+    }
+
---
+++ Strings1/Strings1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Manipulação de Strings", Substring(7, 4);
+        }
+    }
+}
